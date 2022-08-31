19e46e9cd80b6fd929bfe0a446b307c98d907427
angelique-df <faye.angelique.d@gmail.com>
2022-08-31 14:37:31 +0000

    Update topicsReducer.test.ts
    - Remove nested `topic` object from assertion. Assert that we are working with a `topics` object and nothing else.
    If we added a `favouriteTopics`, it would not be included in the state.

--- src/features/topics/topicsReducer.test.ts
+++ src/features/topics/topicsReducer.test.ts
@@ -4,8 +4,11 @@ import { v4 as uuidv4 } from "uuid";
 describe("topicsReducer", () => {
   test("Should return initial state by default ", () => {
     expect(
-      reducer(undefined, { type: undefined, payload: undefined })
-    ).toEqual({ topics: { topics: {} } });
+      reducer(undefined, {
+        type: undefined,
+        payload: undefined
+      })
+    ).toEqual({ topics: {} });
   });
 
   test("Should add a topic", () => {
